1706ced2c640ad93f4adea7eabce550a6b0b8b2b
NikSan <nikulinaleksandr@gmail.com>
2014-03-13 23:27:48 +0400

    upd

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,34 +1,28 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
+    dashboard
+    index
     new
     export
+    history_index
     bulk_delete
     show
     edit
     delete
+    history_show
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+    nestable
   end
+
+  config.main_app_name = ['Aqua-Vrn', 'Admin']
+  config.current_user_method { current_user } # auto-generated
+
+  config.excluded_models = ['Role', 'Ckeditor::Asset', 'Ckeditor::AttachmentFile', 'Ckeditor::Picture']
+
+  [:email, :title].each do |method|
+    config.label_methods << method
+  end
+
+
 end
