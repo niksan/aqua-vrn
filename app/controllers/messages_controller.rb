ae92f02183c7263de0278430c52483f55cf77e7c
NikSan <nikulinaleksandr@gmail.com>
2014-03-16 22:14:53 +0400

    upd

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,12 @@
 class MessagesController < ApplicationController
+
+  def create
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:name, :email, :phone, :body)
+    end
+
 end
